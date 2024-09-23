381a04246e719c43b029a4cf5fbbb5ef2fba6edb
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2024-09-23 12:26:26 -0300

    Add files via upload

---
+++ CustomException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excecaoPersonalizada
+{
+    public class CustomException : Exception
+    {
+        // construtor vazio
+        public CustomException() { }
+
+        // construtor recebe a mensagem e passa a mensagem para o pai (:base)
+        public CustomException(string message) : base(message) { }
+        // construtor que recebe a mensage, o objeto exception e passando para 
+        // a classe pai message e inner (:base)
+        public CustomException(string message, Exception inner) : base(message, inner) { }
+
+        // redefinindo a mensagem da classe pai
+        public override string Message
+        {
+            get
+            {
+                return "Essa é a nova mensagem!";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,13 @@
-﻿try
-{
-    Console.WriteLine("Digite da URL da página: ");
-    string? url = Console.ReadLine();
-    // criando uma requisição 
-    HttpClient client = new();
-    // caputrando a requisição e recebendo o resultado
-    HttpResponseMessage response = client.GetAsync(url).Result;
+﻿using excecaoPersonalizada;
 
-    // Em caso de sucesso
-    if(response.IsSuccessStatusCode)
-    {
-        Console.WriteLine("Acesso bem sucedido");
-        // retorna uma string
-        Console.WriteLine("Código Status" + response.StatusCode);
-    }
-    else
-    {
-        // retorna o código do erro
-        throw new HttpRequestException("Erro:" + (int)response.StatusCode);
-    }
-}// filtrando código erro 404
-catch(HttpRequestException e) when (e.Message.Contains("404"))
+try
 {
-    Console.WriteLine("Página não encontrada " + e.Message);
-}// filtrando código erro 400
-catch(HttpRequestException e) when (e.Message.Contains("400"))
-{
-    Console.WriteLine("Requisição inválida " + e.Message);
-}// capturando outros erros genéricos
-catch(Exception e)
+    throw new CustomException("Minha exceção personalizada!");
+}
+catch(CustomException e)
 {
     Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
 }
 finally
 {
